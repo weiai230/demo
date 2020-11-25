61838655959241b7c1761c108c923fe337e8624f
linzr <522963995@qq.com>
2020-11-25 17:35:16 +0800

    xxxxt 提交测试

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-//1232222
+//1232222ssss
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
