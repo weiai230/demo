7f78c2aedda8aacdb40d110499c2798d5d0c93e8
linzr <522963995@qq.com>
2020-12-16 17:37:56 +0800

    restful风格

--- src/main/java/com/example/demo/Swagger2.java
+++ src/main/java/com/example/demo/Swagger2.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -29,7 +30,7 @@ public class Swagger2 {
                 .title("Spring Boot中使用Swagger2构建RESTful APIs")
                 .description("原文地址链接：http://blog.didispace.com/springbootswagger2/")
                 .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("@我没有三颗心脏")
+                .contact(new Contact("name","url","email"))
                 .version("1.0")
                 .build();
     }
